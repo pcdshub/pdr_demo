9e2caa1de496d1870656227766083a2872396f9b
teddyrendahl <trendahl@slac.stanford.edu>
2019-02-08 16:38:12 -0800

    ENH: Launch Laptop in TyphonSuite

---
+++ show.py
@@ -0,0 +1,29 @@
+import os.path
+
+from qtpy.QtWidgets import QApplication
+from typhon import TyphonSuite, use_stylesheet
+
+from laptop import lp
+
+# Initialize signals
+lp.trigger()
+# Create application
+app = QApplication([])
+use_stylesheet()
+# Create TyphonSuite
+ts = TyphonSuite.from_device(lp)
+# Set our stylesheet
+embedded = os.path.abspath('embedded_screen.ui')
+detailed = os.path.abspath('detailed_screen.ui')
+
+ts.setStyleSheet("""\
+TyphonDeviceDisplay[device_class='laptop.Laptop'][display_type='0']
+    {{qproperty-force_template: '{}';}}
+
+TyphonDeviceDisplay[device_class='laptop.Laptop'][display_type='1']
+    {{qproperty-force_template: '{}';}}
+""".format(embedded, detailed))
+
+# Start the show
+ts.show()
+app.exec_()
